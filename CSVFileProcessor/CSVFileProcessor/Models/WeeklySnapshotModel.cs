a78d70f54ab4db1f8b083a03092e6ae32937d060
skorae <skorae135@gmail.com>
2020-04-06 19:06:27 +0300

    Added interface IWriter.cs and model WeeklySnapshotModel.cs

---
+++ CSVFileProcessor/CSVFileProcessor/Models/WeeklySnapshotModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSVFileProcessor.Models
+{
+    public class WeeklySnapshotModel
+    {
+        public WeeklySnapshotModel()
+        {
+            this.ExternalLinksInfo = new List<string>();
+        }
+
+        public WeeklySnapshotModel(int id, string name, string internalUrl, ICollection<string> externalLinkInfo)
+            : base()
+        {
+            this.Id = id;
+            this.Name = name;
+            this.InternalUrl = internalUrl;
+            this.ExternalLinksInfo = externalLinkInfo;
+            this.Guid = default(Guid);
+            this.Genre = null;
+        }
+
+        public WeeklySnapshotModel(int id, string name, string internalUrl, ICollection<string> externalLinkInfo, string genre)
+            : base()
+        {
+            this.Id = id;
+            this.Name = name;
+            this.InternalUrl = internalUrl;
+            this.ExternalLinksInfo = externalLinkInfo;
+            this.Guid = default(Guid);
+            this.Genre = genre;
+        }
+
+        public WeeklySnapshotModel(int id, string name, string internalUrl, ICollection<string> externalLinkInfo, Guid guid)
+            : base()
+        {
+            this.Id = id;
+            this.Name = name;
+            this.InternalUrl = internalUrl;
+            this.ExternalLinksInfo = externalLinkInfo;
+            this.Guid = guid;
+            this.Genre = null;
+        }
+
+        public WeeklySnapshotModel(int id, string name, string internalUrl, ICollection<string> externalLinkInfo, Guid guid, string genre)
+            : base()
+        {
+            this.Id = id;
+            this.Name = name;
+            this.InternalUrl = internalUrl;
+            this.ExternalLinksInfo = externalLinkInfo;
+            this.Guid = guid;
+            this.Genre = genre;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid Guid { get; set; }
+
+        public string Genre { get; set; }
+
+        public string InternalUrl { get; set; }
+
+        public ICollection<string> ExternalLinksInfo { get; set; }
+    }
+}
---
+++ CSVFileProcessor/CSVFileProcessor/Services/Contracts/IWriter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSVFileProcessor.Services.Contracts
+{
+    public interface IWriter
+    {
+    }
+}
