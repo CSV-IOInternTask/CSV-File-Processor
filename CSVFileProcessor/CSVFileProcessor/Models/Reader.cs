cb701b69068a53888593b31a5efbdf36e4e02864
Ivan Dichev <Ivan.Dichev@endava.com>
2020-04-07 22:08:46 +0300

    Added Reader ctor

---
+++ CSVFileProcessor/CSVFileProcessor/Models/Reader.cs
@@ -0,0 +1,34 @@
+﻿using CSVFileProcessor.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSVFileProcessor.Models
+{
+    class Reader : IReader
+    {
+        public string Directory { get; set; }
+
+        public Reader(string directory)
+        {
+            this.Directory = directory;
+        }
+
+        public ICollection<string> Read(string directory)
+        {
+            using (StreamReader str = new StreamReader(directory))
+            {
+                List<string> data = new List<string>();
+                string line = string.Empty;
+
+                while((line = str.ReadLine()) != null)
+                {
+                    data.Add(line.Split(new char[] { '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToString());
+                }
+                ;
+                return data;
+            }
+        }
+    }
+}
---
+++ CSVFileProcessor/CSVFileProcessor/Services/Contracts/IReader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSVFileProcessor.Services.Contracts
+{
+    interface IReader
+    {
+        public string Directory { get; }
+
+        public ICollection<string> Read(string directory);
+
+
+
+    }
+}
